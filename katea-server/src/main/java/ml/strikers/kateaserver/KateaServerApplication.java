c2601839813c6aa31e14f9073aad02dee9b17aa9
Urzica Vlad <37276343+urzicavlad@users.noreply.github.com>
2019-10-16 02:04:13 +0300

    Adding initial server project with entities. (#3)
    JDK 11.
    Build Tool: Gradle

---
+++ katea-server/src/main/java/ml/strikers/kateaserver/KateaServerApplication.java
@@ -0,0 +1,13 @@
+package ml.strikers.kateaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KateaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KateaServerApplication.class, args);
+    }
+
+}
