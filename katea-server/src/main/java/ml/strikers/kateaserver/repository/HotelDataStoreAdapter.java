3e35b96b3bfc695400de30eee9de3d1c2b0ff3cf
urzica.cristian <cristian.urzica@sotvsion.com>
2019-10-16 17:40:09 +0300

    Adding ssl certs

--- katea-server/src/main/java/ml/strikers/kateaserver/repository/HotelDataStoreAdapter.java
+++ katea-server/src/main/java/ml/strikers/kateaserver/repository/HotelDataStoreAdapter.java
@@ -1,8 +1,13 @@
 package ml.strikers.kateaserver.repository;
 
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.datastore.*;
 import ml.strikers.kateaserver.entity.Hotel;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.UUID;
 
 /**
@@ -15,7 +20,7 @@ public class HotelDataStoreAdapter implements Repository<Hotel, UUID> {
     private Datastore datastore;
     private KeyFactory keyFactory;
 
-    public HotelDataStoreAdapter() {
+    public HotelDataStoreAdapter() throws IOException {
         datastore = DatastoreOptions.getDefaultInstance().getService();
         keyFactory = datastore.newKeyFactory().setKind("Hotels");
     }
