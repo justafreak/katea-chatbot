006dbf0786aed144665b75afbec881b092fa8976
urzica.cristian <cristian.urzica@sotvsion.com>
2019-10-16 16:11:31 +0300

    Adding ssl certs

---
+++ katea-server/src/main/java/ml/strikers/kateaserver/config/TomcatHttpAndHttpsConfig.java
@@ -0,0 +1,26 @@
+package ml.strikers.kateaserver.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpAndHttpsConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
