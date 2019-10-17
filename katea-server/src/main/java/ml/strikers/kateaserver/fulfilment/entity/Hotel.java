e02891312ff0bb29ccd4e511477d1332f9734647
Iulian Balan <iulian.sergiu.balan@macys.com>
2019-10-17 21:06:32 +0300

    Add constructor

--- katea-server/src/main/java/ml/strikers/kateaserver/fulfilment/entity/Hotel.java
+++ katea-server/src/main/java/ml/strikers/kateaserver/fulfilment/entity/Hotel.java
@@ -1,13 +1,17 @@
 package ml.strikers.kateaserver.fulfilment.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Hotel {
 
     public static final String ID = "id";
